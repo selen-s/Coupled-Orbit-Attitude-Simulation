9b429a0ddbfe8735c1a61cecec7a21e82f922827
Selen Serdar <selen.serdar@gmail.com>
2026-03-29 18:43:14 -0400

    Comments

--- coupled_orbit_1d.m
+++ coupled_orbit_1d.m
@@ -1,14 +1,23 @@
-function [] = coupled_orbit_1d(t, sv, masses, J, N)
+%{ sv_derivative: inputs are current time(t) in s, state vector (sv), link
+%masses (masses) in kg, moment of inertia tensor (J) , and number of links
+%(N)
+% outputs 
+%}
+function [sv_derivative] = coupled_orbit_1d(t, sv, masses, J, N)
 
-omega = sv(1:N);
-x = sv(N+1:2*N);
-R = sv(2*N+1:3*N);
-phi = sv(3*N+1:4*N);
-tau = sv(4*N+1:5*N);
+% categorize inputs from state vector.
+w = sv(1:N); % angular velocities (rad/s)
+x = sv(N+1:2*N); % positions (m)
+R = sv(2*N+1:3*N); % rotation matrix 
+phi = sv(3*N+1:4*N); % external forces (N)
+tau = sv(4*N+1:5*N); % torques (Nm)
 
-numelems = 4*N;
-% massM = mass_matrix( ); 
+numelems = 4*N; % number of elements to put in the sparse matrix
 
+% create the matrix of constants:
+% constM = constant_matrix(); 
+massM = mass_matrix(w, x, R, phi, tau, N); 
 
-
+% return the derivative of state vector
+sv_derivative = inv(massM) * constM; 
 end
